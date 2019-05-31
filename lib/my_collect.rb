8d5a455c4826df2147e775680fe2db4838f6663c
Conrad Casper <conradcasper@icloud.com>
2019-05-31 05:12:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(array)
   
   while i < array.length
     yield array[i]
-      new_array << array[i].upcase
+      new_array << array.split(" ").first.upcase
     i += 1
   end
   new_array
